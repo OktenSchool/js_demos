f69587be44c136a0bb521b6dcd423eb71cefb592
GrayHead <ss.zhuravlov@gmail.com>
2024-12-15 17:50:43 +0200

    ts

---
+++ ts/ts-2025/lesson1/main.ts
@@ -0,0 +1,15 @@
+let a: number = 123;
+let b: boolean = true;
+let c: string = 'okten';
+
+// function foo(arg1: number, arg2: number): number {
+//     return arg1 + arg2;
+// }
+//
+// foo(1,'1asdsa');
+
+let arr: any[] = [];
+arr.push(1);
+arr.push('1');
+arr.push(true);
+
